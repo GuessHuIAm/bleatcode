ea0cb237963b407c29781ad5222c25f27948de39
Mike <43020369+GuessHuIAm@users.noreply.github.com>
2021-01-24 23:24:32 -0500

    Update csv.c

--- csv.c
+++ csv.c
@@ -97,3 +97,63 @@ void print_subject(int id){
         fclose(fp);
         return;
 }
+
+char * subject(int id){
+        FILE* fp = fopen("the_problems.csv", "r");
+        if (!fp)
+                printf("Can't open file\n");
+	char buffer[1024];
+       	int row = 0;
+        int column = 0;
+        while (fgets(buffer, 1024, fp)) {
+		if (row == id){
+                	// Turning the ` into new lines
+                	while (strchr(buffer, '`') != NULL){
+				*strchr(buffer, '`') = '\n';
+ 			}
+			// Splitting the data
+			char* value = strtok(buffer, ";");
+			while (value) {
+ 				if (column == 1){
+					fclose(fp);
+					return value;
+				}
+				value = strtok(NULL, ";");
+				column++;
+			}
+		}
+		row++;
+        }
+        fclose(fp);
+        return value;
+}
+
+char * descriptor(int id){
+        FILE* fp = fopen("the_problems.csv", "r");
+        if (!fp)
+                printf("Can't open file\n");
+	char buffer[1024];
+       	int row = 0;
+        int column = 0;
+        while (fgets(buffer, 1024, fp)) {
+		if (row == id){
+                	// Turning the ` into new lines
+                	while (strchr(buffer, '`') != NULL){
+				*strchr(buffer, '`') = '\n';
+ 			}
+			// Splitting the data
+			char* value = strtok(buffer, ";");
+			while (value) {
+ 				if (column == 2){
+					fclose(fp);
+					return value;
+				}
+				value = strtok(NULL, ";");
+				column++;
+			}
+		}
+		row++;
+        }
+        fclose(fp);
+        return value;
+}
