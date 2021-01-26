936fb8cbf7345244b02076fd582e7afb9cad8780
Mike <43020369+GuessHuIAm@users.noreply.github.com>
2021-01-26 04:05:00 -0500

    Update csv.c

--- csv.c
+++ csv.c
@@ -99,11 +99,11 @@ void print_subject(int id){
         return;
 }
 
-char * helper1(int id, int col){ // col = 1->SUBJECT 2->FUNCTIONNAME 3->DESCRIPTOR
+char * helper1(int id, int col, int size){ // col = 1->SUBJECT 2->FUNCTIONNAME 3->DESCRIPTOR
 	FILE* fp = fopen("the_problems.csv", "r");
         if (!fp)
                 printf("Can't open file\n");
-	char buffer[1024];
+	char buffer[size];
        	int row = 0;
         int column = 0;
         while (fgets(buffer, 1024, fp)) {
@@ -130,22 +130,22 @@ char * helper1(int id, int col){ // col = 1->SUBJECT 2->FUNCTIONNAME 3->DESCRIPT
 }
 
 char * subject(int id){
-	return helper1(id, 1);
+	return helper1(id, 1, 32);
 }
 
 char * get_func(int id){
-	return helper1(id, 2);
+	return helper1(id, 2, 32);
 }
 
 char * descriptor(int id){
-	return helper1(id, 3);
+	return helper1(id, 3, 1024);
 }
 
-char * helper2(int id, int col){ // col = 1->RETURNVALUE 2->PARAMETERS 3->%RETURNVALUE 4->TESTCASE1 5->TESTCASE2 6->TESTCASE3
+char * helper2(int id, int col, int size){ // col = 1->RETURNVALUE 2->PARAMETERS 3->%RETURNVALUE 4->TESTCASE1 5->TESTCASE2 6->TESTCASE3
 	FILE* fp = fopen("the_extra_info.csv", "r");
         if (!fp)
                 printf("Can't open file\n");
-	char buffer[1024];
+	char buffer[size];
        	int row = 0;
         int column = 0;
         while (fgets(buffer, 1024, fp)) {
@@ -172,29 +172,29 @@ char * helper2(int id, int col){ // col = 1->RETURNVALUE 2->PARAMETERS 3->%RETUR
 }
 
 char * get_type(int id){
-	return helper2(id, 1);
+	return helper2(id, 1, 32);
 }
 char * get_para(int id){
-	return helper2(id, 2);
+	return helper2(id, 2, 64);
 }
 char * get_type2(int id){
-	return helper2(id, 3);
+	return helper2(id, 3, 32);
 }
 char * get_tc1(int id){
-	return helper2(id, 4);
+	return helper2(id, 4, 32);
 }
 char * get_tc2(int id){
-	return helper2(id, 5);
+	return helper2(id, 5, 32);
 }
 char * get_tc3(int id){
-	return helper2(id, 6);
+	return helper2(id, 6, 32);
 }
 
-char * helper3(int id, int col){ // col = 1->SOLUTION1 2->SOLUTION2 3->SOLUTION3
+char * helper3(int id, int col, int size){ // col = 1->SOLUTION1 2->SOLUTION2 3->SOLUTION3
 	FILE* fp = fopen("the_extra_info.csv", "r");
         if (!fp)
                 printf("Can't open file\n");
-	char buffer[1024];
+	char buffer[size];
        	int row = 0;
         int column = 0;
         while (fgets(buffer, 1024, fp)) {
@@ -221,13 +221,13 @@ char * helper3(int id, int col){ // col = 1->SOLUTION1 2->SOLUTION2 3->SOLUTION3
 }
 
 char * get_ta1(int id){
-	return helper3(id, 1);
+	return helper3(id, 1, 64);
 }
 char * get_ta2(int id){
-	return helper3(id, 3);
+	return helper3(id, 2, 64);
 }
 char * get_ta3(int id){
-	return helper3(id, 3);
+	return helper3(id, 3, 64);
 }
 
 int nextPS(){
