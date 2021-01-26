393c17912b3b5c029514d14a3aa1aa1fc0060833
Mike <43020369+GuessHuIAm@users.noreply.github.com>
2021-01-25 21:03:08 -0500

    Update problemset.h

--- problemset.h
+++ problemset.h
@@ -5,7 +5,7 @@ struct problemset {
 	struct problem * problems[21];
 };
 
-struct problemset *new_set();
+struct problemset *new_set(int id);
 struct problemset *retrieve_set(int id);
 void print_set(char *name, struct problemset *ps);
 void solve(struct problemset *ps, int id);
