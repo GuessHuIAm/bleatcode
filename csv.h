5ff6578076712bc23ad15e08fcf5609e1581eb0e
Mike <43020369+GuessHuIAm@users.noreply.github.com>
2021-01-26 23:10:05 -0500

    Update csv.h

--- csv.h
+++ csv.h
@@ -5,12 +5,12 @@ void print_all();
 void print_descriptor(int id);
 void print_subject(int id);
 
-char * helper1(int id, int col, int size);
+char * helper1(int id, int col);
 char * subject(int id);
 char * get_func(int id);
 char * descriptor(int id);
 
-char * helper2(int id, int col, int size);
+char * helper2(int id, int col);
 char * get_type(int id);
 char * get_para(int id);
 char * get_type2(int id);
@@ -18,10 +18,11 @@ char * get_tc1(int id);
 char * get_tc2(int id);
 char * get_tc3(int id);
 
-char * helper3(int id, int col, int size);
+char * helper3(int id, int col);
 char * get_ta1(int id);
 char * get_ta2(int id);
 char * get_ta3(int id);
+char * get_solution(int id);
 
 int nextPS();
 
